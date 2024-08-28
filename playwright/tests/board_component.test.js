be724cd12d209a990a6776271233cdf15142dd18
Damien Storm <damien.r.storm@gmail.com>
2024-08-28 19:40:49 -0400

    feat: loading test parameters from a yaml config
    - Added 'js-yaml' dependency to 'package.json' and 'package-lock.json'.
    - Created 'test_parameters.yaml' to store test parameters for Playwright tests.
    - Updated 'constants.js' to load test parameters from 'test_parameters.yaml'.
    - Refactored 'board_component.test.js' to use dynamic test parameters from 'constants.js'.

--- playwright/tests/board_component.test.js
+++ playwright/tests/board_component.test.js
@@ -2,55 +2,58 @@ import { test, expect } from '@playwright/test';
 import * as utils from '../helpers/utils';
 import * as constants from '../helpers/constants';
 
-const PAGE_URL = '/?path=/story/board--example';
-const CHECKBOX_LABELS = [
-  'NO_CHECKBOX',
-  'Always Show Details',
-  'Hide Colors',
-  'Include Weekends',
-  'Loading',
-  'Show Trends',
-];
-
 test.describe.configure({ mode: 'parallel' });
 
-test.describe('Board Component: ', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto(PAGE_URL);
-    page.context.storyBookRoot = await utils.waitForStorybookRootToLoad(page);
-  });
+constants.TEST_PARAMS.forEach(testParams => {
+  const PAGE_URL = testParams.page_url;
+  const CHECKBOX_LABELS = testParams.checkbox_labels;
+  const HAS_MESSAGE_FIELD = testParams.has_message_field;
+  test.describe('Board Component: ', () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto(PAGE_URL);
+      page.context.storyBookRoot = await utils.waitForStorybookRootToLoad(page);
+      await page.waitForTimeout(1000);
+    });
 
-  // Test all data sets
-  constants.DATA_SET_VALUES.forEach(dataset => {
-    CHECKBOX_LABELS.forEach(checkboxLabel => {
-      test(`Data Set: ${dataset} | Checkbox: ${checkboxLabel}`, async ({
-        page,
-      }) => {
-        await utils.selectDataSet(page.context.storyBookRoot, dataset);
+    // Test the page with no options selected
+    test('No Options', async ({ page }) => {
+      await expect(page).toHaveScreenshot();
+    });
 
-        if (checkboxLabel === 'NO_CHECKBOX') {
+    // Test all data sets
+    constants.DATA_SET_VALUES.forEach(dataset => {
+      CHECKBOX_LABELS.forEach(checkboxLabel => {
+        test(`Data Set: ${dataset} | Checkbox: ${checkboxLabel}`, async ({
+          page,
+        }) => {
+          await utils.selectDataSet(page.context.storyBookRoot, dataset);
+
+          if (checkboxLabel === 'NO_CHECKBOX') {
+            await expect(page).toHaveScreenshot();
+            return;
+          }
+
+          await utils.setCheckBox(
+            page.context.storyBookRoot,
+            checkboxLabel,
+            true,
+          );
           await expect(page).toHaveScreenshot();
-          return;
-        }
-
-        await utils.setCheckBox(
-          page.context.storyBookRoot,
-          checkboxLabel,
-          true,
-        );
-        await expect(page).toHaveScreenshot();
-        await utils.setCheckBox(
-          page.context.storyBookRoot,
-          checkboxLabel,
-          false,
-        );
+          await utils.setCheckBox(
+            page.context.storyBookRoot,
+            checkboxLabel,
+            false,
+          );
+        });
       });
     });
-  });
 
-  // Test the message functionality
-  test('Message', async ({ page }) => {
-    await utils.setMessageBox(page.context.storyBookRoot, 'Test Message');
-    await expect(page).toHaveScreenshot();
+    // Test the message functionality
+    if (HAS_MESSAGE_FIELD) {
+      test('Message', async ({ page }) => {
+        await utils.setMessageBox(page.context.storyBookRoot, 'Test Message');
+        await expect(page).toHaveScreenshot();
+      });
+    }
   });
 });
