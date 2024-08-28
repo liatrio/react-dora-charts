6f0a3c3662b04c796615bdb23fdc7c4f26077031
Casey Wilson <c@caseyw.com>
2024-08-28 10:27:03 -0400

    chore: initial tests passing and added to code validation (#8)

--- tests/ChangeFailureRate.test.tsx
+++ tests/ChangeFailureRate.test.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import ChangeFailureRateGraph from '../src/ChangeFailureRateGraph';
+import { changeFailureRateName } from '../src/constants';
 
 test('renders component', () => {
   render(<ChangeFailureRateGraph data={[]} />);
-  const element = screen.getByTestId('ChangeFailureRateGraph');
+  const element = screen.getByTestId(changeFailureRateName);
   expect(element).toBeInTheDocument();
 });
--- tests/ChangeLeadTime.test.tsx
+++ tests/ChangeLeadTime.test.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import ChangeLeadTimeGraph from '../src/ChangeLeadTimeGraph';
+import { changeLeadTimeName } from '../src/constants';
 
 test('renders component', () => {
   render(<ChangeLeadTimeGraph data={[]} />);
-  const element = screen.getByTestId('ChangeLeadTimeGraph');
+  const element = screen.getByTestId(changeLeadTimeName);
   expect(element).toBeInTheDocument();
 });
