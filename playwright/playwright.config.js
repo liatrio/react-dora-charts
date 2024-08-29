71796d95e6d12fd40818f1b6509d3b4660eee9f1
Damien Storm <damien.r.storm@gmail.com>
2024-08-29 01:42:26 -0400

    ci: update Playwright configuration and GitHub action
    - Added a name for the Playwright Tests job in the GitHub Actions workflow.
    - Set a timeout of 10 minutes for the Playwright Tests job.
    - Added steps to install Playwright browsers and run Playwright tests in the GitHub Actions workflow.
    - Configured the Playwright web server to use 'npm run storybook-ci' command and 'http://127.0.0.1:6006' URL.

--- playwright/playwright.config.js
+++ playwright/playwright.config.js
@@ -36,9 +36,9 @@ export default defineConfig({
     trace: 'on-first-retry',
   },
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run storybook-ci',
+    url: 'http://127.0.0.1:6006',
+    reuseExistingServer: !process.env.CI,
+  },
 });
