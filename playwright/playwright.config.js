bfaeff730a78c88e32fceb09add62515e6d361d2
Damien Storm <damien.r.storm@gmail.com>
2024-08-29 01:02:06 -0400

    chore: update playwright config with settings from 'init playwright@latest'

--- playwright/playwright.config.js
+++ playwright/playwright.config.js
@@ -1,12 +1,44 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 import * as constants from './helpers/constants';
 
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
 export default defineConfig({
   testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  // Set default action timeout to 2 seconds
+  actionTimeout: 2 * 1000,
+  // Set default navigation timeout to 5 seconds
+  navigationTimeout: 5 * 1000,
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: constants.BASE_URL,
-    fullyParallel: true,
-    actionTimeout: 2 * 1000, // Set default action timeout to 2 seconds
-    navigationTimeout: 5 * 1000, // Set default navigation timeout to 5 seconds
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
   },
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://127.0.0.1:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
