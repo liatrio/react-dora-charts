5fd014efd24d44e383a17137dd57a7a3abaae68c
Jarrett Gaither <jarrett.gaither@liatrio.com>
2025-05-22 12:16:25 -0700

    revert: reverting main back to v1.1.10

--- playwright/helpers/constants.js
+++ playwright/helpers/constants.js
@@ -4,7 +4,7 @@ import yaml from 'js-yaml';
 // Constants
 export const BASE_URL = 'http://localhost:6006';
 export const TEMP_DIR = path.resolve(__dirname, '..', 'temp');
-export const DATA_SET_VALUES = ['Low', 'Medium', 'High', 'Elite', 'Service'];
+export const DATA_SET_VALUES = ['Low', 'Medium', 'High', 'Elite', 'Team'];
 
 // Test Parameters
 const yamlFile = path.resolve(__dirname, '..', 'test_parameters.yaml');
